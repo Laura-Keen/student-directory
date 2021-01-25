267d04239af9876c5fa4dd02177964e49063c698
Laura <keenlaura@hotmail.co.uk>
2021-01-25 16:17:40 +0000

    Added option to load saved students in main program

--- directory_copy.rb
+++ directory_copy.rb
@@ -23,14 +23,10 @@ def input_students
   # while the name is not empty, repeat this code
   while !name.empty? do
     # add the student hash to the array
-    if name.length < 12
-      students << {name: name, cohort: :november}
-      puts "Now we have #{students.count} students"
-      name = gets.chomp
-    else
-      # get another name from the user
-      name = gets.chomp
-    end
+    students << {name: name, cohort: :november}
+    puts "Now we have #{students.count} students"
+    # get another name from the user
+    name = gets.chomp
   end
   # return the array of students
   students
